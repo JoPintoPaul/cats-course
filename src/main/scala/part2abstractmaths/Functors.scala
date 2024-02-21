2e81212d4aebef2fc25892d6de68665360e0309a
JoPintoPaul <joanna.pinto.paul@digital.hmrc.gov.uk>
2024-02-21 12:14:24 +0000

    Functors: Part 2

--- src/main/scala/part2abstractmaths/Functors.scala
+++ src/main/scala/part2abstractmaths/Functors.scala
@@ -31,9 +31,40 @@ object Functors {
 
   def do10xInts[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] = functor.map(container)(_ * 10)
 
+  // Define our own
+  trait Tree[+T]
+  object Tree {
+    // step 2: Using "smart" constructors
+    def leaf[T](value: T): Tree[T] = Leaf(value)
+    def branch[T](value: T, left: Tree[T], right: Tree[T]): Tree[T] = Branch(value, left, right)
+  }
+  case class Leaf[+T](value: T) extends Tree[T]
+  case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+
+  implicit object TreeFunctor extends Functor[Tree] {
+
+    override def map[A, B](tree: Tree[A])(func: A => B): Tree[B] = {
+      tree match {
+        case Leaf(value) => Leaf(func(value))
+        case Branch(value, left, right) => Branch(func(value), map(left)(func), map(right)(func))
+      }
+    }
+  }
+
+  // extension method - map
+  import cats.syntax.functor._
+  val tree: Tree[Int] = Tree.branch(40, Tree.branch(5, Tree.leaf(10), Tree.leaf(30)), Tree.leaf(20))
+  val incrementedTree = tree.map(_ + 1)
+
+  // Note that `F[_]: Functor` means that it is CONTEXT BOUND
+  def do10xShorter[F[_]: Functor](container: F[Int]): F[Int] = container.map(_ * 10)
+
   def main(args: Array[String]): Unit = {
     println(do10xInts(List(1, 2, 3)))
     println(do10xInts(Try(4)))
     println(do10xInts(Option(5)))
+//    println(do10xInts[Tree](Branch(15, Leaf(25), Leaf(35))))
+
+    println(do10xInts(Tree.branch(15, Tree.leaf(25), Tree.leaf(35))))
   }
 }
